5d03030d3d55790a75aad9e4fc17413b8d34abc7
warleon <51892390+warleon@users.noreply.github.com>
2025-09-11 22:05:24 -0500

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -3,6 +3,10 @@ import { withPayload } from '@payloadcms/next/withPayload'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // Your Next.js config here
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  output: 'standalone',
   webpack: (webpackConfig) => {
     webpackConfig.resolve.extensionAlias = {
       '.cjs': ['.cts', '.cjs'],
