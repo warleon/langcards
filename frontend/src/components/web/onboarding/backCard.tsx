87f849e46eb53bfde84931d90753f4c0f6543b78
warleon <51892390+warleon@users.noreply.github.com>
2025-09-17 11:11:56 -0500

    language selection

--- frontend/src/components/web/onboarding/backCard.tsx
+++ frontend/src/components/web/onboarding/backCard.tsx
@@ -1,10 +1,22 @@
 import React from 'react'
 import TitledCard from '@/components/ui/titledCard'
+import { Card } from '@/components/ui/card'
 
 interface SimpleCardProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string
 }
+const LEVELS = ['beginer', 'intermediate', 'advanced', 'native']
 
 export default function BackCard({ title, ...props }: SimpleCardProps) {
-  return <TitledCard title={title} {...props}></TitledCard>
+  return (
+    <TitledCard title={title} {...props}>
+      <div className="grid grid-cols-2 grid-rows-2 gap-2">
+        {LEVELS.map((level) => (
+          <Card key={level} className="">
+            {level}{' '}
+          </Card>
+        ))}
+      </div>
+    </TitledCard>
+  )
 }
