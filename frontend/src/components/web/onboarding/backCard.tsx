563922e15dc7470a20da0f886f4105d8fdceba91
warleon <51892390+warleon@users.noreply.github.com>
2025-10-10 15:41:44 -0500

    Update backCard.tsx

--- frontend/src/components/web/onboarding/backCard.tsx
+++ frontend/src/components/web/onboarding/backCard.tsx
@@ -1,19 +1,25 @@
 import React from 'react'
 import TitledCard from '@/components/ui/titledCard'
 import { Card } from '@/components/ui/card'
+import { LangLevel } from '@/lib/redux/features/onboarding'
 
 interface SimpleCardProps extends React.HTMLAttributes<HTMLDivElement> {
   title: string
 }
-const LEVELS = ['beginer', 'intermediate', 'advanced', 'native']
+const LEVELS: LangLevel[] = ['beginner', 'intermediate', 'advanced', 'native']
 
 export default function BackCard({ title, ...props }: SimpleCardProps) {
   return (
     <TitledCard title={title} {...props}>
       <div className="grid grid-cols-2 grid-rows-2 gap-2">
         {LEVELS.map((level) => (
-          <Card key={level} className="">
-            {level}{' '}
+          <Card
+            key={level}
+            className="hover:cursor-pointer hover:shadow-inner"
+            role="button"
+            onClick={() => {}}
+          >
+            {level}
           </Card>
         ))}
       </div>
